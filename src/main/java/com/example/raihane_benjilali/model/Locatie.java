0e8858738df9162e966ce08613c97001fb50b8fb
RaihaneBenjilali <raihane.benjilali@student.odisee.be>
2025-05-15 09:58:48 +0200

    locatie aangemaakt

--- src/main/java/com/example/raihane_benjilali/model/Locatie.java
+++ src/main/java/com/example/raihane_benjilali/model/Locatie.java
@@ -1,4 +1,50 @@
 package com.example.raihane_benjilali.model;
 
 public class Locatie {
+    private int id;
+    private String naam;
+    private String adres;
+    private int capaciteit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public int getCapaciteit() {
+        return capaciteit;
+    }
+
+    public void setCapaciteit(int capaciteit) {
+        this.capaciteit = capaciteit;
+    }
+
+    public Locatie() {
+    }
+
+    public Locatie(int id, String naam, String adres, int capaciteit) {
+        this.id = id;
+        this.naam = naam;
+        this.adres = adres;
+        this.capaciteit = capaciteit;
+    }
 }
